2e5040cf0e10855f9c84da858e075d86d365bbd8
parobeth <parobeth@8f70f32c-a9c4-11de-ba6e-69e41c8db662>
2009-09-26 16:22:02 +0000

    SVN test

---
+++ src/com/parobeth/openparobeth/HelloWorld.java
@@ -0,0 +1,13 @@
+package com.parobeth.openparobeth;
+
+public class HelloWorld {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+
+	}
+
+}
